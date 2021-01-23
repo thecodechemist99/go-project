92ea10feb4cb86de27cae675420339c0488bb64c
Florian Beck <florian.f.beck@web.de>
2021-01-23 13:58:57 +0100

    reading turned off
    Signed-off-by: Florian Beck <florian.f.beck@web.de>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function checkForTag () {
         gCurrentTag = ret;
         console.log('tagPresented', gCurrentTag);
         writePage(1, 'test');
-        readPage(1);
+        //readPage(1);
     }
 }
 

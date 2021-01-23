cec499cf2ddc752cf3fc8685656ce20f1b594828
Florian Beck <florian.f.beck@web.de>
2021-01-23 13:44:34 +0100

    test commit
    Signed-off-by: Florian Beck <florian.f.beck@web.de>

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function checkForTag () {
 function readPage (page) {
     rc522.readPage(page, (err, retVal) => {
         if (err === 0) {
-
         }
         console.log(retVal, err);
     });
@@ -35,7 +34,6 @@ function writePage (page, str) {
     console.log(`Attempting to write ${data} ...`)
     rc522.writePage(page, data, (err, retVal) => {
         if (err === 0) {
-
         }
         console.log(retVal, err);
     });

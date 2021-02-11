b4cb380ea8cec16d8f82f536ced7643be52d4457
Florian Beck <florian.f.beck@web.de>
2021-02-11 16:16:18 +0100

    test
    Signed-off-by: Florian Beck <florian.f.beck@web.de>

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function checkIn (tagId) {
     console.log('check in');
     if (tagId != 2) {
         const res = await queryDatabase(`INSERT INTO journey_log(token_id, station_id) values('${tagId}', ${stationId})`);
-        console.log(JSON.stringify(res));
+        console.log(JSON.parse(JSON.stringify(res)));
         const result = res.toString();
         console.log(result);
         if (result.includes('OkPacket')) {

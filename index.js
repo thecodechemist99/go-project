d94afba484da5c104afdc9f9c2f7debe680d1b72
Florian Beck <florian.f.beck@web.de>
2021-02-11 16:50:44 +0100

    db conn test
    Signed-off-by: Florian Beck <florian.f.beck@web.de>

--- index.js
+++ index.js
@@ -154,10 +154,12 @@ async function queryDatabase (query) {
         conn = await pool.getConnection();
         const res = await conn.query(query);
 	    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+        return res;
     } catch (err) {
         console.error(`Error querying database: ${err}`);
     } finally {
-        if (conn) return conn.end();
+        conn.end();
+//        if (conn) return conn.end();
     }
 }
 

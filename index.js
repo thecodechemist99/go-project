6ecb9ccb2889d7cac3286d8b85ca4e95983dd539
Florian Beck <florian.f.beck@web.de>
2021-02-11 16:05:38 +0100

    db JSON test
    Signed-off-by: Florian Beck <florian.f.beck@web.de>

--- index.js
+++ index.js
@@ -90,14 +90,14 @@ async function checkIn (tagId) {
     if (tagId != 2) {
         const res = await queryDatabase(`INSERT INTO journey_log(token_id, station_id) values('${tagId}', ${stationId})`);
         console.log(JSON.stringify(res));
-        console.log(JSON.parse(JSON.stringify(res)));
-        console.log(JSON.parse(JSON.stringify(res)).warningStatus);
-        if (JSON.parse(JSON.stringify(res)).warningStatus) {
-            blink('red');
-        } else {
-            blink('green');
-        }
-    }
+//        console.log(JSON.parse(JSON.stringify(res)));
+//        console.log(JSON.parse(JSON.stringify(res)).warningStatus);
+//        if (JSON.parse(JSON.stringify(res)).warningStatus) {
+//            blink('red');
+//        } else {
+//            blink('green');
+//        }
+//    }
 }
 
 async function checkOut (tagId) {
